2b37adb81de61731bce526cb39d002f7ac19bb15
Chuck Hagenbuch <chuck@horde.org>
2013-08-20 22:00:41 -0400

    Add Apache license to BSD-owned files

--- example_list_constituent_groups.php
+++ example_list_constituent_groups.php
@@ -1,5 +1,21 @@
 <?php
 /**
+ * Copyright 2013 Blue State Digital
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * This example will return a list of all of the constituent groups in your BSD
  * Tools. Fill in your API ID, API secret, and the base URL of your BSD install
  * (including /page/api/ at the end), and you should be able to execute it out
